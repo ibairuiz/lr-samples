924265e9619e80febd67823eb8a7c8b9229e5eb7
Eduardo Pérez García <eduardo.perez@liferay.com>
2017-09-07 17:11:23 +0200

    [#7] Create query

--- modules/dynamic-query-sample/src/main/java/dynamic/query/sample/portlet/DynamicQuerySamplePortlet.java
+++ modules/dynamic-query-sample/src/main/java/dynamic/query/sample/portlet/DynamicQuerySamplePortlet.java
@@ -1,12 +1,28 @@
 package dynamic.query.sample.portlet;
 
+import com.liferay.portal.kernel.dao.orm.Disjunction;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import dynamic.query.sample.constants.DynamicQuerySamplePortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author eduardo
@@ -26,4 +42,34 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class DynamicQuerySamplePortlet extends MVCPortlet {
+
+	GroupLocalService groupLocalService;
+
+	@Override
+	public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+
+		List<Group> sites = new ArrayList();
+
+		DynamicQuery query = groupLocalService.dynamicQuery();
+
+		query.add(PropertyFactoryUtil.forName("site").eq(true));
+
+		Disjunction disjunction = RestrictionsFactoryUtil.disjunction();
+		disjunction.add(PropertyFactoryUtil.forName("type").eq(GroupConstants.TYPE_SITE_OPEN));
+		disjunction.add(PropertyFactoryUtil.forName("type").eq(GroupConstants.TYPE_SITE_PRIVATE));
+
+		query.add(disjunction);
+
+		sites = groupLocalService.dynamicQuery(query); // query -> SELECT * from GROUP_ WHERE site=TRUE AND (type='1' OR type='3')
+
+		renderRequest.setAttribute("sites", sites);
+
+		super.doView(renderRequest, renderResponse);
+	}
+
+	@Reference (unbind = "-")
+	public void setGroupLocalService(GroupLocalService groupLocalService) {
+		this.groupLocalService = groupLocalService;
+	}
+
 }
